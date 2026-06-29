d375eb78b3efbea684a2344afa512714ca1c884f
Arindam-Harh <arindamharh96@gmail.com>
2026-06-29 08:41:55 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (78.44%) - LeetSync

---
+++ 1033-broken-calculator/broken-calculator.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int brokenCalc(int startValue, int target) {
+        int steps = 0;
+        while(startValue < target){
+            if(target % 2 == 0) target /= 2;
+            else target += 1;
+            steps++;
+        }
+        return startValue - target + steps;
+    }
+}
