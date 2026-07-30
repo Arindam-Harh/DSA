08e2db8755d3ea23fdf88c7b347d5ec46de22775
Arindam-Harh <arindamharh96@gmail.com>
2026-07-30 11:42:09 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (43.57%) - LeetSync

--- 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.java
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.java
@@ -1,24 +1,11 @@
 class Solution {
     public int balancedStringSplit(String s) {
-        int Rcount = 0;
-        int Lcount = 0;
+        int count = 0;
         int str = 0;
-        if (s.charAt(0) == 'R') Rcount = 1;
-        else Lcount = 1;
-        if (Rcount == 1) {
-            for (int i = 1; i < s.length(); i++) {
-                if (s.charAt(i) == 'R') Rcount++;
-                else Rcount--;
-
-                if (Rcount == 0) str++;
-            }
-        }else{
-            for(int i=1;i<s.length();i++){
-                if(s.charAt(i) == 'L') Lcount++;
-                else Lcount--;
-
-                if(Lcount == 0) str++;
-            }   
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == 'R') count++;
+            else count--;
+            if(count == 0) str++;
         }
         return str;
     }
