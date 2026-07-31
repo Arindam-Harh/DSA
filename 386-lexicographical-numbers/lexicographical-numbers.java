29f9cd673d65a11a02bd0a0ca340c3e08a09973f
Arindam-Harh <arindamharh96@gmail.com>
2026-08-01 01:16:18 +0530

    Time: 4 ms (75.38%) | Memory: 50.4 MB (22.02%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.java
+++ 386-lexicographical-numbers/lexicographical-numbers.java
@@ -2,12 +2,9 @@ class Solution {
     public List<Integer> lexicalOrder(int n) {
         List<Integer> list = new ArrayList<>();
         for(int i=1;i<=9;i++){
-            fun(i, n, list);
+            dfs(i, n, list);
         }
         return list;
-    }
-    static void fun(int curr, int high,  List<Integer> list){
-        dfs(curr, high, list);
     }
     static void dfs(int curr, int high, List<Integer> list){
         if(curr > high) return;
