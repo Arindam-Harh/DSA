7f08dc6d1278344f612d7b6b0b568d9d803221a5
Arindam-Harh <arindamharh96@gmail.com>
2026-08-01 01:27:17 +0530

    Time: 4 ms (75.38%) | Memory: 50.5 MB (13.91%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.java
+++ 386-lexicographical-numbers/lexicographical-numbers.java
@@ -10,8 +10,8 @@ class Solution {
         if(curr > high) return;
         list.add(curr);
         for(int digit=0;digit<=9;digit++){
-            int next = curr*10 + digit;
-            dfs(next,  high, list);
+            int next = curr * 10 + digit;
+            dfs(next, high, list);
         }
     }
 }
