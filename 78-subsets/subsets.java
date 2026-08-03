600d83f06f6eef3779957002cbe8c1a25ec4bb49
Arindam-Harh <arindamharh96@gmail.com>
2026-08-03 23:59:36 +0530

    Time: 1 ms (86.83%) | Memory: 44.3 MB (39.58%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,7 +1,8 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> list = new ArrayList<>();
-        subset(nums, 0,  new ArrayList<>(), list);
+        // List<List<Integer>> list = new ArrayList<>();
+        // subset(nums, 0,  new ArrayList<>(), list);
+        List<List<Integer>> list = subsetIte(nums);
         return list;
     }
     static void subset(int[] arr,int i, List<Integer> curr, List<List<Integer>> list){
@@ -13,5 +14,18 @@ class Solution {
         curr.add(arr[i]);
         subset(arr, i+1, curr, list);
         curr.remove(curr.size() - 1);
+    }
+    static List<List<Integer>> subsetIte(int[] arr){
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+        for(int num : arr){
+            int n = outer.size();
+            for(int i=0;i<n;i++){
+                List<Integer> internal = new ArrayList<>(outer.get(i));
+                internal.add(num);
+                outer.add(internal);
+            }
+        }
+        return outer;
     }
 }
