3ff309477fe386cc623a53270141d540121b3d86
Arindam-Harh <arindamharh96@gmail.com>
2026-08-03 20:02:56 +0530

    Time: 1 ms (51.21%) | Memory: 45.6 MB (23.69%) - LeetSync

--- 825-max-increase-to-keep-city-skyline/max-increase-to-keep-city-skyline.java
+++ 825-max-increase-to-keep-city-skyline/max-increase-to-keep-city-skyline.java
@@ -4,20 +4,15 @@ class Solution {
         int[] row = new int[n];
         int[] col = new int[n];
         for(int i=0;i<n;i++){
-            int maxVal = grid[i][0];
-            int mVal = grid[0][i];
             for(int j=0;j<n;j++){
-                maxVal = Math.max(maxVal, grid[i][j]);
-                mVal = Math.max(mVal, grid[j][i]);
+                row[i]= Math.max(row[i], grid[i][j]);
+                col[j] = Math.max(col[j], grid[i][j]);
             }
-            row[i] = maxVal;
-            col[i] = mVal;
         }
         int score = 0;
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                int m = Math.min(row[i], col[j]);
-                score += m - grid[i][j];
+                score += Math.min(row[i], col[j]) - grid[i][j];
             }
         }
         return score;
