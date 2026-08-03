306e96e5b27988ec6db8b88fbc3cf6cec5de154a
Arindam-Harh <arindamharh96@gmail.com>
2026-08-04 00:32:36 +0530

    Time: 2 ms (99.72%) | Memory: 45 MB (60.82%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,23 +1,20 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        return subset(nums);
-    }
-    static List<List<Integer>> subset(int[] arr){
-        Arrays.sort(arr);
+        Arrays.sort(nums);
         List<List<Integer>> outer = new ArrayList<>();
         outer.add(new ArrayList<>());
         int start = 0;
         int end = 0;
-        for(int i=0;i<arr.length;i++){
+        for(int i=0;i<nums.length;i++){
             start = 0;
-            if(i>0 && arr[i] == arr[i-1]){
+            if(i>0 && nums[i] == nums[i-1]){
                 start = end + 1;
             }
             end = outer.size() - 1;
             int n = outer.size();
             for(int j=start;j<n;j++){
                 List<Integer> internal = new ArrayList<>(outer.get(j));
-                internal.add(arr[i]);
+                internal.add(nums[i]);
                 outer.add(internal);
             }
         }
