cb113ae720b6759194314f876fc72a39d065e146
Arindam-Harh <arindamharh96@gmail.com>
2026-08-05 12:41:59 +0530

    Time: 5 ms (6.21%) | Memory: 46.8 MB (5.05%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,20 +1,20 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-        return parenthesis(n, n*2, "", 0, 0);
+        return parenthesis(n, "", 0, 0);
     }
-    static List<String> parenthesis(int limit, int n, String p, int open, int close){
-        if(n == 0){
+    static List<String> parenthesis(int limit, String p, int open, int close){
+        if(open == limit && close == limit){
             List<String> list = new ArrayList<>();
             list.add(p);
             return list;
         }
-        List<String> l = new ArrayList();
+        List<String> l = new ArrayList<>();
         if(open < limit){
-            List<String> l1 = parenthesis(limit, n-1, p + '(', open+1, close);
+            List<String> l1 = parenthesis(limit, p + '(', open+1, close);
             l.addAll(l1);
         }
         if(open > close){
-            List<String> l2 = parenthesis(limit, n-1, p + ')', open, close+1);
+            List<String> l2 = parenthesis(limit, p + ')', open, close+1);
             l.addAll(l2);
         }
         return l;
