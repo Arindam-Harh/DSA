e1becf7431453ea27ae314849f70a8997349630f
Arindam-Harh <arindamharh96@gmail.com>
2026-08-05 12:44:05 +0530

    Time: 3 ms (14.60%) | Memory: 46.6 MB (5.05%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,8 +1,26 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-        return parenthesis(n, "", 0, 0);
+        return parenthesis1(n, n*2, "", 0, 0);
+        // return parenthesis2(n, "", 0, 0);
     }
-    static List<String> parenthesis(int limit, String p, int open, int close){
+    static List<String> parenthesis1(int limit, int n, String p, int open, int close){
+        if(n == 0){
+            List<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        List<String> l = new ArrayList<>();
+        if(open < limit){
+            List<String> l1 = parenthesis1(limit, n-1, p + '(', open+1, close);
+            l.addAll(l1);
+        }
+        if(open > close){
+            List<String> l2 = parenthesis1(limit, n-1, p + ')', open, close+1);
+            l.addAll(l2);
+        }
+        return l;
+    }
+    static List<String> parenthesis2(int limit, String p, int open, int close){
         if(open == limit && close == limit){
             List<String> list = new ArrayList<>();
             list.add(p);
@@ -10,11 +28,11 @@ class Solution {
         }
         List<String> l = new ArrayList<>();
         if(open < limit){
-            List<String> l1 = parenthesis(limit, p + '(', open+1, close);
+            List<String> l1 = parenthesis2(limit, p + '(', open+1, close);
             l.addAll(l1);
         }
         if(open > close){
-            List<String> l2 = parenthesis(limit, p + ')', open, close+1);
+            List<String> l2 = parenthesis2(limit, p + ')', open, close+1);
             l.addAll(l2);
         }
         return l;
