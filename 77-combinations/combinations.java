970d07391be106e6dc01b4c5fc73a48399b3e1e4
Arindam-Harh <arindamharh96@gmail.com>
2026-08-07 12:31:11 +0530

    Time: 18 ms (80.18%) | Memory: 98.2 MB (27.75%) - LeetSync

--- 77-combinations/combinations.java
+++ 77-combinations/combinations.java
@@ -1,8 +1,7 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> list = new ArrayList<Integer>();
-        combinations(1, list, n, k, ans);
+        combinations(1, new ArrayList<>(), n, k, ans);
         return ans;
     }
     static void combinations(int start, List<Integer> p, int n, int size, List<List<Integer>> ans){
