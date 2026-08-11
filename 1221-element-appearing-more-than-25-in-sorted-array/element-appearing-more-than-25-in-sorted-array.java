4ec81f4ddf460d78a87f383ec680d19ff93aa7da
Arindam-Harh <arindamharh96@gmail.com>
2026-08-11 11:51:12 +0530

    Time: 1 ms (66.56%) | Memory: 46.5 MB (27.52%) - LeetSync

--- 1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.java
+++ 1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.java
@@ -4,7 +4,7 @@ class Solution {
         int count = 1;
         for(int i=0;i<arr.length-1;i++){
             if(arr[i] == arr[i+1]) count += 1;
-            if((double)count / arr.length > 0.25) return arr[i];
+            if(count * 4 > arr.length) return arr[i];
             if(arr[i] != arr[i+1]) count = 1;
         }
         return 0;
