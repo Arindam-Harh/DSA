df8f862ced6e618e21d83963a20340293a0f5640
Arindam-Harh <arindamharh96@gmail.com>
2026-08-12 11:07:46 +0530

    Time: 16 ms (38.26%) | Memory: 53.6 MB (14.96%) - LeetSync

--- 491-non-decreasing-subsequences/non-decreasing-subsequences.java
+++ 491-non-decreasing-subsequences/non-decreasing-subsequences.java
@@ -5,12 +5,8 @@ class Solution {
         return new ArrayList<>(set);
     }
     static void permute(int start, List<Integer> p,int[] nums,HashSet<List<Integer>> set){
-        if(start == nums.length){
-            if(p.size() < 2) return;
-            set.add(new ArrayList<>(p));
-            return;
-        }
         if (p.size() >= 2) set.add(new ArrayList<>(p));
+        if(start == nums.length) return;
         for(int i=start;i<nums.length;i++){
             int e = nums[i];
             if(p.size() > 0 && p.get(p.size()-1) > e) continue;
