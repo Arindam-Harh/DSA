ff0d89f4e2749eae631a0fe56947827c1934a23d
Arindam-Harh <arindamharh96@gmail.com>
2026-08-14 13:03:05 +0530

    Time: 4 ms (95.21%) | Memory: 46.2 MB (86.32%) - LeetSync

--- 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.java
+++ 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.java
@@ -3,7 +3,6 @@ class Solution {
     public int numSub(String s) {
         long count = 0;
         long ans = 0;
-
         for(char ch : s.toCharArray()){
             if(ch == '1') count++;
             else {
@@ -11,7 +10,6 @@ class Solution {
                 count = 0;
             }
         }
-        ans = (ans + count*(count+1)/2) % MOD;
-        return (int) ans;
+        return (int) (ans + count*(count+1)/2) % MOD;
     }
 }
