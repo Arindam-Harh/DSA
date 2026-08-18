ad05bcdfbaf87859ca409222536736603ab5a300
Arindam-Harh <arindamharh96@gmail.com>
2026-08-18 12:18:25 +0530

    Time: 16 ms (5.28%) | Memory: 46.3 MB (87.05%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.java
+++ 692-top-k-frequent-words/top-k-frequent-words.java
@@ -1,19 +1,32 @@
 class Solution {
     public List<String> topKFrequent(String[] words, int k) {
+        Arrays.sort(words);
         HashMap<String, Integer> map = new HashMap<>();
+        HashSet<String> set = new HashSet<>();
+        List<String> w = new ArrayList<>();
         for(String s : words){
             map.put(s, map.getOrDefault(s, 0) + 1);
+            if(!set.contains(s)) {
+                w.add(s);
+                set.add(s);
+            }
         }
-        TreeSet<String> set = new TreeSet<>((a, b) -> {
-            if(!map.get(a).equals(map.get(b)))
-                return map.get(b) - map.get(a);
-            return a.compareTo(b);
-        });
-        set.addAll(map.keySet());
+        List<Integer> list = new ArrayList<>();
+        for(var entry : map.entrySet()){
+            list.add(entry.getValue());
+        }
+        Collections.sort(list, Collections.reverseOrder());
         List<String> ans = new ArrayList<>();
-        while( k > 0){
-            ans.add(set.pollFirst());
-            k--;
+        while( ans.size() < k){
+            for (int j = 0; j < w.size(); j++) {
+                if (map.get(w.get(j)).equals(list.get(0))) {
+                    ans.add(w.get(j));
+                    if(ans.size() == k) break;
+                    list.remove(0);
+                    w.remove(j);
+                    break;
+                }
+            }
         }
         return ans;
     }
