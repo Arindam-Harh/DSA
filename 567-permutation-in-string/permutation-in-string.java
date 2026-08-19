acb28da533aa37509b67a826618f73a0e5b65704
Arindam-Harh <arindamharh96@gmail.com>
2026-08-19 15:48:52 +0530

    Time: 26 ms (25.64%) | Memory: 46.9 MB (14.68%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,8 +1,9 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
+        if(s1.length() > s2.length()) return false;
         int n1 = s1.length();
         int n2 = s2.length();
-        if(n1 > n2) return false;
+        boolean permute = true;
         HashMap<Character, Integer> map1 = new HashMap<>();
         HashMap<Character, Integer> map2 = new HashMap<>();
         for(char c : s1.toCharArray()){
@@ -12,14 +13,26 @@ class Solution {
             char c = s2.charAt(i);
             map2.put(c, map2.getOrDefault(c, 0) + 1);
         }
-        if(map1.equals(map2)) return true;
+        for(var entry : map1.entrySet()){
+            if(!Objects.equals(map2.get(entry.getKey()), entry.getValue())){
+                permute = false;
+                break;
+            }
+        }
+        if(permute) return true;
         for(int i=1;i<=n2-n1;i++){
+            permute = true;
             char remove = s2.charAt(i-1);
-            if(map2.get(remove) == 1) map2.remove(remove);
-            else map2.put(remove, map2.get(remove) - 1);
+            map2.put(remove, map2.getOrDefault(remove, 0) - 1);
             char add = s2.charAt(i+n1-1);
             map2.put(add, map2.getOrDefault(add, 0) + 1);
-            if(map1.equals(map2)) return true;
+            for(var entry : map1.entrySet()){
+                if(!Objects.equals(map2.get(entry.getKey()), entry.getValue())){
+                    permute = false;
+                    break;
+                }
+            }
+            if(permute) return true;
         }
         return false;
     }
