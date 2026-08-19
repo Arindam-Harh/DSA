de3b251e3f44fc5d6a83784af9bd622cd670cd2a
Arindam-Harh <arindamharh96@gmail.com>
2026-08-19 10:44:45 +0530

    Time: 58 ms (23.10%) | Memory: 48.1 MB (6.71%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -3,17 +3,36 @@ class Solution {
         int n = s.length();
         int l = p.length();
         if(n < l) return  new ArrayList<>();
-        p = sorted(p);
         List<Integer> idx = new ArrayList<>();
-        for(int i=0;i<=n-l;i++){
-            String str = s.substring(i, i+l);
-            if(p.equals(sorted(str))) idx.add(i);
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char c : p.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        HashMap<Character, Integer> map1 = new HashMap<>();
+        for(int i=0;i<l;i++){
+            char c = s.charAt(i);
+            map1.put(c, map1.getOrDefault(c, 0) + 1);
+        }
+        boolean isAnagram = true;
+        for(var entry : map.entrySet()){
+            if(!Objects.equals(map1.get(entry.getKey()), entry.getValue())){
+                isAnagram = false;
+            } 
+        }
+        if(isAnagram) idx.add(0);
+        for(int i=1;i<=n-l;i++){
+            isAnagram = true;
+            char remove = s.charAt(i - 1);
+            map1.put(remove, map1.getOrDefault(remove, 0) - 1);
+            char add = s.charAt(i + l - 1);
+            map1.put(add, map1.getOrDefault(add, 0) + 1);
+            for(var entry : map.entrySet()){
+                if(!Objects.equals(map1.get(entry.getKey()), entry.getValue())){
+                    isAnagram = false;
+                } 
+            }
+            if(isAnagram) idx.add(i);
         }
         return idx;
-    }
-    static String sorted(String s){
-        char[] arr = s.toCharArray();
-        Arrays.sort(arr);
-        return new String(arr);
     }
 }
