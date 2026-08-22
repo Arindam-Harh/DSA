3756c9e1e977c94e95574d7469c61dad640c8a7b
Arindam-Harh <arindamharh96@gmail.com>
2026-08-22 15:34:01 +0530

    Time: 1 ms (98.35%) | Memory: 42.6 MB (61.54%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -4,14 +4,11 @@ class Solution {
         int s = 0;
         int e = x/2;
         while(s <= e){
-            int m = (s + e)/2;
-            if((long)m*m == x){
-                return m;
-            }else if((long)m*m > x){
-                e = m-1;
-            }else{
-                s = m+1;
-            }
+            int m = e + (s-e)/2;
+            long sq = (long) m*m;
+            if(sq == x) return m;
+            else if(sq> x) e = m-1;
+            else s = m+1;
         }
         return e;
     }
