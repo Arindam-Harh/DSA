7a56af77693273b6cacd5fd864516e96088ab590
Arindam-Harh <arindamharh96@gmail.com>
2026-08-25 19:57:38 +0530

    Time: 1573 ms (6.74%) | Memory: 280.8 MB (5.28%) - LeetSync

---
+++ 900-reordered-power-of-2/reordered-power-of-2.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean reorderedPowerOf2(int n) {
+        List<Integer> ans = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        while(n > 0){
+            int r = n%10;
+            list.add(r);
+            n /= 10;
+        }
+        fun(new ArrayList<>(), list, ans, list.size());
+        for(int i=0;i<ans.size();i++){
+            n = ans.get(i);
+            if(n > 0 && (n & (n - 1)) == 0) return true;
+        }
+        return false;
+    }
+    static void fun(List<Integer> p, List<Integer> up, List<Integer> ans, int n){
+        if(up.isEmpty()){
+            if(p.get(0) != 0) ans.add(toNumber(p));
+            return;
+        }
+        int e = up.get(0);
+        for(int i=0;i<=p.size();i++){
+            List<Integer> newList = new ArrayList<>(p);
+            newList.add(i, e);
+            fun(newList, up.subList(1, up.size()), ans, n);
+        } 
+    }
+    static int toNumber(List<Integer> list) {
+        int num = 0;
+        for (int x : list) {
+            num = num * 10 + x;
+        }
+        return num;
+    }
+}
