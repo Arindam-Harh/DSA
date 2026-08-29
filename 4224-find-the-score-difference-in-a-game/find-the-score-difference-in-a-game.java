f9513d11edbd5a466c4cf432078c34742545bfa1
Arindam-Harh <arindamharh96@gmail.com>
2026-08-29 20:06:08 +0530

    Time: 1 ms (100.00%) | Memory: 46.9 MB (51.84%) - LeetSync

--- 4224-find-the-score-difference-in-a-game/find-the-score-difference-in-a-game.java
+++ 4224-find-the-score-difference-in-a-game/find-the-score-difference-in-a-game.java
@@ -1,15 +1,14 @@
 class Solution {
     public int scoreDifference(int[] nums) {
-        int p1 = 0;
-        int p2 = 0;
+        int score = 0;
         boolean active = true;
         for(int i=0;i<nums.length;i++){
             if(nums[i]%2 != 0) active = !active;
             if((i+1)%6 == 0) active = !active;
 
-            if(active) p1 += nums[i];
-            else p2 += nums[i];
+            if(active) score += nums[i];
+            else score -= nums[i];
         }
-        return p1 - p2;
+        return score;
     }
 }
