91451cf5dfddb866c514d2b014374897e59d336a
Arindam-Harh <arindamharh96@gmail.com>
2026-09-03 20:24:29 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (58.38%) - LeetSync

---
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        // logic : we have any two cases
+        // 1. either all values are even or odd in that case we dont have do anything
+        // 2. values are odd and even combined, then making odd from even and odd is very easy when i != j, 
+        // so the answer is always be true.
+        return true;
+    }
+}
