7437ffc3e4e171759d08faddf735d208d409b358
Arindam-Harh <arindamharh96@gmail.com>
2026-09-04 10:11:39 +0530

    Time: 5 ms (83.81%) | Memory: 121.6 MB (68.47%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.java
@@ -3,14 +3,10 @@ class Solution {
         int minOdd = Integer.MAX_VALUE;
         int minEven = Integer.MAX_VALUE;
         for(int n : nums1){
-            if(n%2 != 0){
-                minOdd = Math.min(n, minOdd);
-            }else{
-                minEven = Math.min(n, minEven);
-            }
+            if(n%2 != 0) minOdd = Math.min(n, minOdd);
+            else minEven = Math.min(n, minEven);
         }
         if(minOdd == Integer.MAX_VALUE || minEven == Integer.MAX_VALUE) return true;
-        if(minOdd >= minEven) return false;
-        return true;
+        return !(minOdd >= minEven);
     }
 }
