231b7bd6a9699fc2a71ccaee1cd2f99a6a89949d
Arindam-Harh <arindamharh96@gmail.com>
2026-09-05 01:03:54 +0530

    Time: 32 ms (87.83%) | Memory: 125.9 MB (92.17%) - LeetSync

--- 4078-maximum-alternating-sum-of-squares/maximum-alternating-sum-of-squares.java
+++ 4078-maximum-alternating-sum-of-squares/maximum-alternating-sum-of-squares.java
@@ -2,14 +2,13 @@ class Solution {
     public long maxAlternatingSum(int[] nums) {
         int n = nums.length;
         for(int i=0;i<n;i++){
-            nums[i] = nums[i] < 0 ? -nums[i] : nums[i];
+            nums[i] = nums[i] * nums[i];
         }
         Arrays.sort(nums);
         long ans = 0;
         for(int i=0;i<n;i++){
-            long sq = (long) Math.pow(nums[i], 2);
-            if(i < n/2) ans -= sq;
-            else ans += sq;
+            if(i < n/2) ans -= nums[i];
+            else ans += nums[i];
         }
         return ans;
     }
