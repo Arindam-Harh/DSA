d06112a2b00040dcb142abff7518ecdb719c5227
Arindam-Harh <arindamharh96@gmail.com>
2026-09-06 11:27:24 +0530

    Time: 74 ms (6.17%) | Memory: 48.6 MB (6.65%) - LeetSync

---
+++ 876-hand-of-straights/hand-of-straights.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+        if(hand.length % groupSize != 0) return false;
+        Arrays.sort(hand);
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for(int n : hand){
+            map.put(n, map.getOrDefault(n, 0)+1);
+        }
+        while(!map.isEmpty()){
+            int first = map.firstKey();
+            for(int i=0;i<groupSize;i++){
+                int next = first + i;
+                if(!map.containsKey(next)) return false;
+                map.put(next, map.get(next)-1);
+                if(map.get(next) == 0) map.remove(next);
+            }
+        }
+        return true;
+    }
+}
