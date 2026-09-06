2858ba7c9e60dc513f4838cd647f8734bd1d87ce
Arindam-Harh <arindamharh96@gmail.com>
2026-09-06 11:38:59 +0530

    Time: 66 ms (20.13%) | Memory: 47.5 MB (92.79%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.java
+++ 876-hand-of-straights/hand-of-straights.java
@@ -7,7 +7,9 @@ class Solution {
         }
         while(!map.isEmpty()){
             int first = map.firstKey();
-            for(int i=0;i<groupSize;i++){
+            map.put(first, map.get(first)-1);
+            if(map.get(first) == 0) map.remove(first);
+            for(int i=1;i<groupSize;i++){
                 int next = first + i;
                 if(!map.containsKey(next)) return false;
                 map.put(next, map.get(next)-1);
