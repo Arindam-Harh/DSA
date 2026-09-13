3d1e4901e16896dbe8c6fe5787bc01f719786506
Arindam-Harh <arindamharh96@gmail.com>
2026-09-13 19:49:54 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (85.74%) - LeetSync

--- 376-wiggle-subsequence/wiggle-subsequence.java
+++ 376-wiggle-subsequence/wiggle-subsequence.java
@@ -7,13 +7,11 @@ class Solution {
         for(int i=0;i<nums.length-1;i++){
             if(nums[i+1] > nums[i]){
                 up = true;
-                if(down && i==0) count += 2;
-                else if(down) count++; //taking only one down
+                if(down) count++; //taking only one down
                 down = false;
             }else if(nums[i+1] < nums[i]){
                 down = true;
-                if(up && i==0) count += 2;
-                else if(up) count++; // taking only one up
+                if(up) count++; // taking only one up
                 up = false;
             }else  continue;
         }
