75b6fe6b65d2417c520fd8c461d3d82f847d35a9
Arindam-Harh <arindamharh96@gmail.com>
2026-09-13 19:44:05 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (54.79%) - LeetSync

--- 376-wiggle-subsequence/wiggle-subsequence.java
+++ 376-wiggle-subsequence/wiggle-subsequence.java
@@ -1,36 +1,20 @@
 class Solution {
     public int wiggleMaxLength(int[] nums) {
         if(nums.length <= 1) return 1;
-        int count = 0;
+        int count = 1;
         boolean up = false;
         boolean down = false;
         int i = 0;
-        if(nums[1] < nums[0]) {
-            down = true;
-            count += 2;
-        }else if(nums[1] > nums[0]) {
-            up = true;
-            count += 2;
-        }else {
-            while(i < nums.length-1 && nums[i] == nums[i+1]){
-                i++;
-            }
-            if(i < nums.length-1 && nums[i+1] > nums[i]){
-                up = true;
-                count += 2;
-            }else if(i < nums.length-1 && nums[i+1] < nums[i]){
-                down = true;
-                count += 2;
-            }else count++;
-        }
         while(i<nums.length-1){
             if(nums[i+1] > nums[i]){
                 up = true;
-                if(down) count++; //taking only one down
+                if(down && i==0) count += 2;
+                else if(down) count++; //taking only one down
                 down = false;
             }else if(nums[i+1] < nums[i]){
                 down = true;
-                if(up) count++; // taking only one up
+                if(up && i==0) count += 2;
+                else if(up) count++; // taking only one up
                 up = false;
             }else {
                 i++;
@@ -38,6 +22,7 @@ class Solution {
             };
             i++;
         }
-        return count;
+        if(!down && !up) return count;
+        return count + 1;
     }
 }
