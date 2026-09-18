de1923d7999093e1448ba15be1d811cb00cd393f
Arindam-Harh <arindamharh96@gmail.com>
2026-09-18 10:25:43 +0530

    Time: 4 ms (21.35%) | Memory: 42.8 MB (66.44%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.java
+++ 807-custom-sort-string/custom-sort-string.java
@@ -1,17 +1,15 @@
 class Solution {
     public String customSortString(String order, String s) {
         StringBuilder sb = new StringBuilder();
-        LinkedHashMap<Character, Integer> map = new LinkedHashMap<>();
+        HashMap<Character, Integer> map = new HashMap<>();
         for(char ch : s.toCharArray()){
             map.put(ch, map.getOrDefault(ch, 0)+1);
         }
         for(char ch : order.toCharArray()){
-            if(map.containsKey(ch)){
-                while(map.containsKey(ch)){
-                    sb.append(ch);
-                    map.put(ch, map.get(ch)-1);
-                    if(map.get(ch) == 0) map.remove(ch);
-                }
+            while(map.containsKey(ch)){
+                sb.append(ch);
+                map.put(ch, map.get(ch)-1);
+                if(map.get(ch) == 0) map.remove(ch);
             }
         }
         while(!map.isEmpty()){
