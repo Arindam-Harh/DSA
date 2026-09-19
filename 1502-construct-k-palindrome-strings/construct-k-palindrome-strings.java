75b7348de90f5dc1a59145bd09844b9db3672906
Arindam-Harh <arindamharh96@gmail.com>
2026-09-19 20:27:34 +0530

    Time: 5 ms (48.09%) | Memory: 47.4 MB (61.20%) - LeetSync

--- 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.java
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.java
@@ -8,15 +8,8 @@ class Solution {
             arr[c-'a'] = arr[c-'a'] == -1 ? 1 : arr[c-'a']+1;
         }
         int odd = 0;
-        int even = 0;
         for(int i=0;i<26;i++){
-            if(arr[i] != -1) {
-                if(arr[i]%2 == 0) even++;
-                else{
-                    if(arr[i] >= 3) even += arr[i]/2;
-                    odd++;
-                }
-            }
+            if(arr[i] != -1 && arr[i]%2 != 0) odd++;
         }
         if(odd > k) return false;
         return true;
