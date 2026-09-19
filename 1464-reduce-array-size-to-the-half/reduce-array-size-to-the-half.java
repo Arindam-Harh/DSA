e055a66c4d930250c119ff35cca288781dd9f7fc
Arindam-Harh <arindamharh96@gmail.com>
2026-09-19 23:56:37 +0530

    Time: 42 ms (29.60%) | Memory: 88.5 MB (28.55%) - LeetSync

---
+++ 1464-reduce-array-size-to-the-half/reduce-array-size-to-the-half.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minSetSize(int[] arr) {
+        int l = arr.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int n : arr){
+            map.put(n, map.getOrDefault(n, 0)+1);
+        }
+        int m = map.size();
+        if(m == 1) return 1;
+        int a[][] = new int[m][2];
+        int i = 0;
+        for(var entry : map.entrySet()){
+            a[i][0] = entry.getKey();
+            a[i][1] = entry.getValue();
+            i++;
+        }
+        Arrays.sort(a, (x, y) -> Integer.compare(x[1], y[1]));
+        int j = m-1;
+        int target = l/2;
+        int count = 0;
+        while(j > 0){
+            l -= a[j][1];
+            count++;
+            j--;
+            if(l <= target) return count;
+        }
+        return -1;
+    }
+}
